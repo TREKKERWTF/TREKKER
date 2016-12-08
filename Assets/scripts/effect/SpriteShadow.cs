639787238a28511e94173f821a23c1c7c5e0beef
light-zhao <2568252796@qq.com>
2016-12-08 09:57:13 +0800

    0.0.1
    nothing

---
+++ Assets/scripts/effect/SpriteShadow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections;
+
+public class SpriteShadow : MonoBehaviour {
+	public ShadowCastingMode castShadow=ShadowCastingMode.Off;
+	public bool receiveShadow=false;
+	SpriteRenderer sprr;
+	void Awake(){
+		sprr = GetComponent<SpriteRenderer> ();
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnEnable()  
+	{  
+		sprr.shadowCastingMode = castShadow;
+		sprr.receiveShadows = receiveShadow;
+	}  
+}
